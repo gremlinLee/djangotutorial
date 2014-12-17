ad4caaa6d11168b79207144cacbd937447e018ec
gremlin_lee <gremlin.leen@gmail.com>
2014-12-17 23:57:00 +0200

    Added 13 homework.

--- pybursa_courses/pybursa_courses/urls.py
+++ pybursa_courses/pybursa_courses/urls.py
@@ -6,8 +6,8 @@ from pybursa_courses import view
 urlpatterns = patterns('',
 
     url(r'^$', view.index, name='index'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^students/', include('students.urls')),
-    url(r'^courses/', include('courses.urls')),
-    url(r'^coaches/', include('coaches.urls')),
+    url(r'^admin/', include(admin.site.urls), name='admin'),
+    url(r'^students/', include('students.urls'), name='students'),
+    url(r'^courses/', include('courses.urls'), name='courses'),
+    url(r'^coaches/', include('coaches.urls'), name='coaches'),
 )
