9ab7bbc0a420afc7bd7c61a66c9333b4856d7ada
gremlin_lee <gremlin.leen@gmail.com>
2014-12-21 23:51:56 +0200

    Added 14 homework, but I need help.

--- pybursa_courses/students/views.py
+++ pybursa_courses/students/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views import generic
+from django import forms
 
 from students.models import Student
 
@@ -14,6 +16,36 @@ class StudentListView(generic.ListView):
         print student.name
         print student.surname
         return Student.objects.order_by('-name')
+
+
+class StudentForm(forms.Form):
+    PACKAGE_CHOICES = (
+        ('standart', 'Standart'),
+        ('gold', 'Gold'),
+        ('vip', 'VIP'),
+    )
+    student_name = forms.CharField(max_length=100)
+    student_package = forms.ChoiceField(
+        choices=PACKAGE_CHOICES,
+        widget=forms.RadioSelect)
+    student_note = forms.CharField(widget=forms.Textarea)
+
+
+def student_save(request):
+    if request.method is 'post':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            print form.cleaned_data
+            return redirect('student_list')
+        else:
+            form = StudentForm()
+
+        return render(request, 'students/new_student.html', {'form': form})
+
+
+def student_add(request):
+    form = StudentForm()
+    return render(request, 'students/new_student.html', {'form': form})
 
 
 def concrete_student(request, student_id):
